17c9cb54a1cfd6814fff84cd168b45f77b45fe55
Sanjay Puri <purisanjay007@gmail.com>
2024-04-28 17:08:15 +0530

    second progress commit

--- app.js
+++ app.js
@@ -1,17 +1,30 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const notifier = require('node-notifier');
 const db = require('./database');
 const { render } = require('ejs');
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(cookieParser());
 app.set('view engine', 'ejs');
 
 const PORT = 5000;
 
 app.get('/', (request, response) => {
-  let sql = `SELECT * FROM portfolios`;
+  // notifier.notify('Message')
+  const userData = request.cookies.userData;
+  if (!userData) {
+    response.redirect('/selectportfolio')
+  } else {
+    response.render('home', { userData });
+  }
+});
+
+app.get('/selectportfolio', (request, response) => {
+  const sql = `SELECT * FROM portfolios`;
   db.all(sql, (error, portfolios) => {
     if (error) {
       const errormsg = {
@@ -21,14 +34,32 @@ app.get('/', (request, response) => {
       response.render('errorpage', { errormsg });
     } else {
       if (portfolios.length < 1) {
-        const errormsg = {
-          type: "No portfolios available.",
-          details: "Please create a portfolio first..."
-        }
-        response.render('errorpage', { errormsg });
+        response.redirect('/newportfolio');
       } else {
-        response.render('home', { portfolios });
+        if (portfolios.length === 1) {
+          response.cookie('userData', { 'id': portfolios[0].id, 'name': portfolios[0].name });
+          response.redirect('/');
+        } else {
+          response.render('selectportfolio', { portfolios })
+        }
       }
+    }
+  });
+});
+
+app.post('/selectportfolio', (request, response) => {
+  const portfolioid = parseInt(request.body.portfolio);
+  const sql = `SELECT * FROM portfolios WHERE id = ?`;
+  db.get(sql, portfolioid, (error, portfolio) => {
+    if (error) {
+      const errormsg = {
+        type: "Error reading Portfolio",
+        details: error.message
+      }
+      response.render('errorpage', { errormsg });
+    } else {
+      response.cookie('userData', { id: portfolio.id, name: portfolio.name });
+      response.redirect('/');
     }
   });
 });
@@ -62,33 +93,50 @@ app.post('/newportfolio', (request, response) => {
       }
       response.render('errorpage', { errormsg });
     } else {
-      response.redirect('/');
+      response.redirect('/selectportfolio');
     }
   });
 });
 
-app.post('/showportfolio', (request, response) => {
-  const portfolioid = request.body.portfolio;
-  let sql = `select *, 
-              sum(daygain) over (partition by portfolioid) tdaygain,
-              sum(totalgain) over (partition by portfolioid) ttotalgain
-              from
-              (
-                select 
-              	b.portfolioid, p.name portfolioname, b.id, b.shareid, sl.name, sl.price, b.qty buyQty, (b.rate*b.qty)+b.brokrage buyvalue, ((b.rate*b.qty)+b.brokrage)/b.qty buyavg, 
-	              s.qty sellqty, s.value sellvalue, s.avg sellavg, b.qty-s.qty qtyinhand, (b.qty-s.qty)* (((b.rate*b.qty)+b.brokrage)/b.qty) buycost,
-              	sl.price*(b.qty-s.qty) marketvalue, sl.temp changepercent, sl.change*( b.qty-s.qty) daygain, (sl.price-(((b.rate*b.qty)+b.brokrage)/b.qty))*(b.qty-s.qty) totalgain
-              from buy b 
-              left join 
-            (select 
-            	buyid, sum(qty) qty, sum(rate*qty)-sum(brokrage) value, (sum(rate*qty)-sum(brokrage))/sum(qty) avg     
-              from sell 
-            group by buyid) s on b.id = s.buyid
-            left join shareslist sl on sl.id = b.shareid
-            left join portfolios p on p.id = b.portfolioid
-            where b.qty-s.qty > 0 and b.portfolioid = ?
-            order by sl.name)`;
-  db.all(sql, portfolioid, (error, rows) => {
+app.get('/showportfolio', (request, response) => {
+  const userData = request.cookies.userData;
+  let sql = `select
+  *,
+	buyqty-sellqty qtyinhand,
+  sum(daygain) over  (partition by portfolioid) tdaygain,
+  sum(gain) over  (partition by portfolioid) tgain,
+  sum(marketvalue) over (partition by portfolioid) tvalue
+  from
+  (
+    select
+    f.shareid, ${userData.id} portfolioid, sl.name,
+    sum(f.buyqty) buyqty, (sum(f.buyqty*f.buyrate)+sum(f.buybrokrage))/sum(f.buyqty) buyavg, 
+    sum(f.buyqty*f.buyrate)+sum(f.buybrokrage) buycost, sum(f.sellqty) sellqty, 
+    ifnull((sum(f.sellqty*f.sellrate)+sum(f.sellbrokrage))/sum(f.sellqty),0) sellavg,
+    sl.price, (sum(f.buyqty)-sum(f.sellqty))*sl.price marketvalue, sl.temp changepercent, 
+    (sl.price-sl.previous)*(sum(f.buyqty)-sum(f.sellqty)) daygain,
+    (sl.price-((sum(f.buyqty*f.buyrate)+sum(f.buybrokrage))/sum(f.buyqty)))*(sum(f.buyqty)-sum(f.sellqty)) gain
+    from
+    (
+      select
+      b.portfolioid, b.shareid, b.qty buyqty, b.rate buyrate, b.brokrage buybrokrage,
+      ifnull(ss.qty,0) sellqty, ifnull(ss.rate,0) sellrate, ifnull(ss.brokrage,0) sellbrokrage
+      from buy b
+      left join 
+      (
+        select s.buyid, sum(s.qty) qty, sum(s.qty*s.rate)/sum(s.qty) rate, sum(s.brokrage) brokrage
+        from sell s
+        group by s.buyid
+      ) ss on ss.buyid = b.id
+      where portfolioid = ? and buyqty-sellqty > 0
+    ) f
+    left join shareslist sl on sl.id = f.shareid
+    group by f.shareid
+  )
+  where qtyinhand > 0
+  order by name
+`;
+  db.all(sql, userData.id, (error, rows) => {
     if (error) {
       const errormsg = {
         type: "Error computing portfolio",
@@ -96,104 +144,21 @@ app.post('/showportfolio', (request, response) => {
       }
       response.render('errorpage', { errormsg });
     } else {
-      if (rows > 1) {
-        console.log(rows);
-        response.render('portfolio', { rows });
+      if (rows.length > 0) {
+        response.render('showportfolio', { rows, userData });
       } else {
-        let sql = `SELECT *,
-                  sum(daygain) over (partition by portfolioid) tdaygain,
-                  sum(totalgain) over (partition by portfolioid) ttotalgain
-                  from (
-                  SELECT b.portfolioid, p.name portfolioname, b.id, b.shareid, sl.name, sl.price, b.qty buyQty,
-                  (b.rate*b.qty)+b.brokrage buyvalue, ((b.rate*b.qty)+b.brokrage)/b.qty buyavg, 0 sellQty, 0 sellvalue,
-                  0 sellavg, b.qty qtyinhand, (b.qty)* (((b.rate*b.qty)+b.brokrage)/b.qty) buycost, 
-                  sl.price*(b.qty) marketvalue, sl.temp changepercent, sl.change*( b.qty) daygain, 
-                  (sl.price-(((b.rate*b.qty)+b.brokrage)/b.qty))*b.qty totalgain FROM buy b
-                  LEFT JOIN portfolios p ON p.id = b.portfolioid 
-                  LEFT JOIN shareslist sl ON sl.id = b.shareid
-                  WHERE portfolioid = ?
-                  ORDER BY sl.name)`;
-        db.all(sql, portfolioid, (error, rows) => {
-          if (error) {
-            const errormsg = {
-              type: "Error computing buy only portfolio",
-              details: error.message
-            }
-            response.render('errorpage', { errormsg });
-          } else {
-            console.log(rows);
-            response.render('portfolio', { rows });
-          }
-        });
-      }
-    }
-  });
-})
-
-app.get('/showportfolio/:portfolioid', (request, response) => {
-  const portfolioid = request.params.portfolioid;
-  let sql = `select *, 
-              sum(daygain) over (partition by portfolioid) tdaygain,
-              sum(totalgain) over (partition by portfolioid) ttotalgain
-              from
-              (
-                select 
-              	b.portfolioid, p.name portfolioname, b.id, b.shareid, sl.name, sl.price, b.qty buyQty, (b.rate*b.qty)+b.brokrage buyvalue, ((b.rate*b.qty)+b.brokrage)/b.qty buyavg, 
-	              s.qty sellqty, s.value sellvalue, s.avg sellavg, b.qty-s.qty qtyinhand, (b.qty-s.qty)* (((b.rate*b.qty)+b.brokrage)/b.qty) buycost,
-              	sl.price*(b.qty-s.qty) marketvalue, sl.temp changepercent, sl.change*( b.qty-s.qty) daygain, (sl.price-(((b.rate*b.qty)+b.brokrage)/b.qty))*(b.qty-s.qty) totalgain
-              from buy b 
-              left join 
-            (select 
-            	buyid, sum(qty) qty, sum(rate*qty)-sum(brokrage) value, (sum(rate*qty)-sum(brokrage))/sum(qty) avg     
-              from sell 
-            group by buyid) s on b.id = s.buyid
-            left join shareslist sl on sl.id = b.shareid
-            left join portfolios p on p.id = b.portfolioid
-            where b.qty-s.qty > 0 and b.portfolioid = ?
-            order by sl.name)`;
-  db.all(sql, portfolioid, (error, rows) => {
-    if (error) {
-      const errormsg = {
-        type: "Error computing portfolio",
-        details: error.message
-      }
-      response.render('errorpage', { errormsg });
-    } else {
-      if (rows > 1) {
-        console.log(rows);
-        response.render('portfolio', { rows });
-      } else {
-        let sql = `SELECT *,
-                  sum(daygain) over (partition by portfolioid) tdaygain,
-                  sum(totalgain) over (partition by portfolioid) ttotalgain
-                  from (
-                  SELECT b.portfolioid, p.name portfolioname, b.id, b.shareid, sl.name, sl.price, b.qty buyQty,
-                  (b.rate*b.qty)+b.brokrage buyvalue, ((b.rate*b.qty)+b.brokrage)/b.qty buyavg, 0 sellQty, 0 sellvalue,
-                  0 sellavg, b.qty qtyinhand, (b.qty)* (((b.rate*b.qty)+b.brokrage)/b.qty) buycost, 
-                  sl.price*(b.qty) marketvalue, sl.temp changepercent, sl.change*( b.qty) daygain, 
-                  (sl.price-(((b.rate*b.qty)+b.brokrage)/b.qty))*b.qty totalgain FROM buy b
-                  LEFT JOIN portfolios p ON p.id = b.portfolioid 
-                  LEFT JOIN shareslist sl ON sl.id = b.shareid
-                  WHERE portfolioid = ?
-                  ORDER BY sl.name)`;
-        db.all(sql, portfolioid, (error, rows) => {
-          if (error) {
-            const errormsg = {
-              type: "Error computing buy only portfolio",
-              details: error.message
-            }
-            response.render('errorpage', { errormsg });
-          } else {
-            console.log(rows);
-            response.render('portfolio', { rows });
-          }
-        });
+        const errormsg = {
+          type: "Empty Portfolio",
+          details: "No shares are available in this portfolio"
+        }
+        response.render('errorpage', { errormsg });
       }
     }
   });
 })
 
 app.get('/buy', (request, response) => {
+  const userData = request.cookies.userData;
   let sql = `SELECT id, name from shareslist order by name`;
   db.all(sql, (error, shares) => {
     if (error) {
@@ -203,18 +168,7 @@ app.get('/buy', (request, response) => {
       }
       response.render('errorpage', { errormsg });
     } else {
-      let sql = `SELECT id, name FROM portfolios`;
-      db.all(sql, (error, portfolios) => {
-        if (error) {
-          const errormsg = {
-            type: "Error loading Portfolios List",
-            details: error.message
-          }
-          response.render('errorpage', { errormsg });
-        } else {
-          response.render('buy', { shares, portfolios });
-        }
-      });
+      response.render('buy', { shares, userData });
     }
   });
 });
@@ -225,7 +179,7 @@ app.post('/buy', (request, response) => {
   const qty = parseInt(request.body.qty);
   const rate = parseFloat(request.body.rate);
   const brokrage = parseFloat(request.body.brokrage);
-  const portfolioid = parseInt(request.body.selectPortfolio);
+  const portfolioid = parseInt(request.body.portfolioid);
   const sql = `INSERT INTO buy (tdate, shareid, qty, rate, brokrage, portfolioid) VALUES (?, ?, ?, ?, ?, ?)`;
   db.run(sql, [tdate, shareid, qty, rate, brokrage, portfolioid], (error) => {
     if (error) {
@@ -235,48 +189,152 @@ app.post('/buy', (request, response) => {
       }
       response.render('errorpage', { errormsg });
     } else {
-      response.redirect(`/showportfolio/${portfolioid}`);
+      response.redirect(`/showportfolio`);
     }
   });
 });
 
-app.get('/selllist', (request, response)=>{
-  const sql = `SELECT
-	b.portfolioid, p.name portfolioname, b.id, b.shareid, b.tdate purdate, sl.Name Share, b.qty-ifnull(s.qty,0) qtyinhand, b.rate
-  FROM buy b
-  LEFT JOIN portfolios p ON p.id = b.portfolioid
-  LEFT JOIN shareslist sl ON sl.id = b.shareid
-  LEFT JOIN
-    (
-      SELECT buyid, sum(qty) qty FROM sell GROUP BY buyid
-    ) s ON s.buyid = b.id`;
-  db.all(sql, (error, shares)=>{
-    if(error){
+app.get('/selllist', (request, response) => {
+  const userData = request.cookies.userData;
+  const sql = `   SELECT * FROM 
+                  (
+                  SELECT
+                  b.portfolioid, p.name portfolioname, b.id, b.shareid, b.tdate purdate, 
+                  sl.Name Share, b.qty-ifnull(s.qty,0) qtyinhand, b.rate
+                  FROM buy b
+                  LEFT JOIN portfolios p ON p.id = b.portfolioid
+                  LEFT JOIN shareslist sl ON sl.id = b.shareid
+                  LEFT JOIN
+                    (
+                      SELECT buyid, sum(qty) qty FROM sell GROUP BY buyid
+                    ) s ON s.buyid = b.id
+                  )
+                  WHERE qtyinhand > 0 AND portfolioid = ${userData.id}`;
+  db.all(sql, (error, shares) => {
+    if (error) {
       const errormsg = {
         type: "Error retriving list of saleable Shares",
         details: error.message
       }
       response.render('errorpage', { errormsg });
-    } else{
-      response.render('selllist', {shares});
+    } else {
+      response.render('selllist', { shares });
     }
   });
 });
 
-app.get('/sell/:buyid/:qtyinhand', (request, response)=>{
+app.get('/sell/:buyid/:qtyinhand', (request, response) => {
   const buyid = parseInt(request.params.buyid);
   const qtyinhand = parseInt(request.params.qtyinhand);
-  const sql = `SELECT * FROM buy WHERE id = ?`;
-  db.get(sql,  buyid, (error, row)=>{
-    if(error){
+  const sql = `SELECT b.shareid, sl.Name sharename, b.portfolioid, p.name portfolioname FROM buy b
+                LEFT JOIN shareslist sl ON sl.id = b.shareid 
+                LEFT JOIN portfolios p ON p.id = b.portfolioid 
+                WHERE b.id = ?`;
+  db.get(sql, buyid, (error, row) => {
+    if (error) {
       const errormsg = {
         type: "Error retriving share details for buyID",
         details: error.message
       }
       response.render('errorpage', { errormsg });
     } else {
-      response.send({buyid, qtyinhand});
+      response.render('sell', { row, buyid, qtyinhand });
+    }
+  });
+});
 
+app.post('/sell', (request, response) => {
+  const buyid = parseInt(request.body.buyid);
+  const tdate = request.body.date;
+  const qty = parseInt(request.body.qty);
+  const rate = parseFloat(request.body.rate);
+  const brokrage = parseFloat(request.body.brokrage);
+  const portfolioid = parseInt(request.body.portfolioid);
+  const sql = `INSERT INTO sell (buyid, tdate, qty, rate, brokrage) VALUES (?, ?, ?, ?, ?)`;
+  db.run(sql, [buyid, tdate, qty, rate, brokrage], (error) => {
+    if (error) {
+      const errormsg = {
+        type: "Error saving new sales",
+        details: error.message
+      }
+      response.render('errorpage', { errormsg });
+    } else {
+      response.redirect(`/showportfolio`);
+    }
+  });
+});
+
+app.get('/buyregister', (request, response) => {
+  const userData = request.cookies.userData;
+    const sql = `SELECT
+                    b.tdate,
+                    sl.name,
+                    b.qty,
+                    b.rate,
+                    b.brokrage,
+                    b.qty*b.rate buyvalue,
+                    (b.qty*b.rate)+b.brokrage buycost,
+                    ((b.qty*b.rate)+b.brokrage)/b.qty buyavg
+                  FROM buy b
+                  LEFT JOIN shareslist sl on sl.id = b.shareid
+                  WHERE portfolioid = ?
+                  ORDER BY b.tdate DESC`;
+  db.all(sql, userData.id, (error, rows) => {
+    if (error) {
+      const errormsg = {
+        type: "Error reading Purchase Register",
+        details: error.message
+      }
+      response.render('errorpage', { errormsg });
+    } else {
+      if (rows.length > 0) {
+        response.render('buyregister', { rows, userData });
+      } else {
+        const errormsg = {
+          type: "Purchase Register Empty",
+          details: "No entries available in the Purchase Register"
+        }
+        response.render('errorpage', { errormsg });
+      }
+    }
+  });
+});
+
+app.get('/sellregister', (request, response) => {
+  const userData = request.cookies.userData;
+    const sql = `SELECT
+                    s.tdate,
+                    sl.Name,
+                    s.qty,
+                    s.rate,
+                    s.brokrage,
+                    s.qty*s.rate sellvalue,
+                    (s.qty*s.rate)-s.brokrage sellcost,
+                    ((s.qty*s.rate)-b.brokrage)/s.qty sellavg,
+                    (s.rate-b.rate)*s.qty gain,
+                    SUM((s.rate-b.rate)*s.qty) OVER (PARTITION BY b.portfolioid) tgain
+                    FROM sell s
+                  LEFT JOIN buy b ON b.id = s.buyid
+                  LEFT JOIN shareslist sl on sl.id = b.shareid
+                  WHERE b.portfolioid = ?
+                  ORDER BY s.tdate DESC`;
+  db.all(sql, userData.id, (error, rows) => {
+    if (error) {
+      const errormsg = {
+        type: "Error reading Sale Register",
+        details: error.message
+      }
+      response.render('errorpage', { errormsg });
+    } else {
+      if (rows.length > 0) {
+        response.render('saleregister', { rows, userData });
+      } else {
+        const errormsg = {
+          type: "Sale Register Empty",
+          details: "No entries available in the Sale Register"
+        }
+        response.render('errorpage', { errormsg });
+      }
     }
   });
 });
